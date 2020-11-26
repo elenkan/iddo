be5479a568acd693855512d3b4388d6e69f19de2
Елена <elenalecs@rambler.ru>
2020-11-26 14:22:54 +0400

    Добавление нового синтаксиса

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,36 @@
-"use strict";
+'use strict';
 
-var gulp = require("gulp");
-var less = require("gulp-less");
-var plumber = require("gulp-plumber");
-var sourcemap = require("gulp-sourcemaps");
-var postcss = require("gulp-postcss");
-var autoprefixer = require("autoprefixer");
-var server = require("browser-sync").create();
-gulp.task("css", function () {
-	return gulp.src("source/less/style.less")
+const gulp = require('gulp');
+const less = require('gulp-less');
+const plumber = require('gulp-plumber');
+const sourcemap = require('gulp-sourcemaps');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const server = require('browser-sync').create();
+gulp.task('css', function () {
+	return gulp.src('source/less/style.less')
 	.pipe(plumber())
 	.pipe(sourcemap.init())
 	.pipe(less())
 	.pipe(postcss([
 		autoprefixer()]
 		))
-	.pipe(sourcemap.write("."))
-	.pipe(gulp.dest("source/css"));
+	.pipe(sourcemap.write('.'))
+	.pipe(gulp.dest('source/css'));
 }
 	);
-gulp.task("server", function() {
+gulp.task('server', function() {
 	server.init({
-		server: "source/"
+		server: 'source/'
 	});
 	gulp
-  .watch("source/less/**/*.less", gulp.series("css"))
-  .on("change", server.reload);
+  .watch('source/less/**/*.less', gulp.series('css'))
+  .on('change', server.reload);
 	gulp
-  .watch("source/*.html")
-  .on("change", server.reload);
+  .watch('source/*.html')
+  .on('change', server.reload);
   gulp
-  .watch("source/js/**/*.js")
-  .on("change", server.reload);
+  .watch('source/js/**/*.js')
+  .on('change', server.reload);
 });
-gulp.task("start", gulp.series("css","server"));
+gulp.task('start', gulp.series('css','server'));
